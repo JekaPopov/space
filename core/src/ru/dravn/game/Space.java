941fb036d679ef5f29faf399a350ba18fe39138c
JekaPopov <jeka_1108@mail.ru>
2018-02-09 17:02:09 +0300

    летает и стреляет

--- core/src/ru/dravn/game/Space.java
+++ core/src/ru/dravn/game/Space.java
@@ -5,29 +5,56 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 public class Space extends ApplicationAdapter {
+
 	SpriteBatch batch;
-	Texture img;
-	
+	Background background;
+	Hero hero;
+
+	static boolean isAndroid = false;
+	static int ScreenHeight = 720;
+	static int ScreenWidth = 1280;
+
+	float dt;
+
+
+
+
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("background.png");
+		background = new Background();
+		hero =new Hero();
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		dt = Gdx.graphics.getDeltaTime();
+
+		update(dt);
+		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+
+		background.render(batch);
+		hero.render(batch);
+		BulletEmitter.getInstance().render(batch);
+
 		batch.end();
 	}
-	
+
+	private void update(float dt) {
+
+		background.update(hero, dt);
+		hero.update(dt);
+		BulletEmitter.getInstance().update(dt);
+	}
+
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
+
 	}
 }
