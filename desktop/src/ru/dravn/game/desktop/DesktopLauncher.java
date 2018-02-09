3911b08dfa2ed4efc2cf3ecc009181699c1ded38
JekaPopov <jeka_1108@mail.ru>
2018-02-09 12:45:04 +0300

    запускается

---
+++ desktop/src/ru/dravn/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ru.dravn.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ru.dravn.game.Space;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Space(), config);
+	}
+}
---
+++ ios/src/ru/dravn/game/IOSLauncher.java
@@ -0,0 +1,22 @@
+package ru.dravn.game;
+
+import org.robovm.apple.foundation.NSAutoreleasePool;
+import org.robovm.apple.uikit.UIApplication;
+
+import com.badlogic.gdx.backends.iosrobovm.IOSApplication;
+import com.badlogic.gdx.backends.iosrobovm.IOSApplicationConfiguration;
+import ru.dravn.game.Space;
+
+public class IOSLauncher extends IOSApplication.Delegate {
+    @Override
+    protected IOSApplication createApplication() {
+        IOSApplicationConfiguration config = new IOSApplicationConfiguration();
+        return new IOSApplication(new Space(), config);
+    }
+
+    public static void main(String[] argv) {
+        NSAutoreleasePool pool = new NSAutoreleasePool();
+        UIApplication.main(argv, null, IOSLauncher.class);
+        pool.close();
+    }
+}
